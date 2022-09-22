3e8992d1d588eceb4e06d7082d5708585290f5c6
Calvin Tran <calvinhtr@gmail.com>
2022-09-22 00:48:39 -0400

    Fix some grammar in documentation

--- snap.py
+++ snap.py
@@ -8,6 +8,7 @@ class TableParser():
     '''
     def __init__(self, input_data: str, num_cols: int, delimiters: list) -> None:
         split = self.mult_split(input_data, delimiters)
+        # Should probably add some sort of input validity check here
         # Only iterate through a multiple of num_cols, so that any non-filled row is removed
         all_data = [split[x:x+num_cols] for x in range(0, len(split)-len(split)%num_cols, num_cols)]
         self.data = all_data[1:]
@@ -51,7 +52,7 @@ class TableParser():
     def fix_flight_codes(self):
         for i in range(len(self.df)):
             if self.df.loc[i, 'FlightCodes'] == '':
-                # Check if previous and following entry in 'FlightCodes' column is valid
+                # Check if previous and following entry in 'FlightCodes' column are valid
                 if self.df.loc[i-1, 'FlightCodes'] != '' and self.df.loc[i+1, 'FlightCodes'] != '' and int(float(self.df.loc[i-1, 'FlightCodes'])) == int(float(self.df.loc[i+1, 'FlightCodes']))-20:
                     self.df.loc[i, 'FlightCodes'] = int(float(self.df.loc[i-1, 'FlightCodes'])) + 10
         # Convert column to int
@@ -63,11 +64,11 @@ class TableParser():
     def print_table(self):
         print(self.df)
 
-
-data = 'Airline Code;DelayTimes;FlightCodes;To_From\nAir Canada (!);[21, 40];20015.0;WAterLoo_NEWYork\n<Air France> (12);[];;Montreal_TORONTO\n(Porter Airways. );[60, 22, 87];20035.0;CALgary_Ottawa\n12. Air France;[78, 66];;Ottawa_VANcouvER\n""".\\.Lufthansa.\\.""";[12, 33];20055.0;london_MONTreal\n'
-t = TableParser(data, 4, [';','\n'])
-t.to_upper('To_From')
-t.remove_special_chars('Airline Code')
-t.fix_flight_codes()
-t.format_to_from()
-t.print_table()
+if __name__ == "__main__":
+    data = 'Airline Code;DelayTimes;FlightCodes;To_From\nAir Canada (!);[21, 40];20015.0;WAterLoo_NEWYork\n<Air France> (12);[];;Montreal_TORONTO\n(Porter Airways. );[60, 22, 87];20035.0;CALgary_Ottawa\n12. Air France;[78, 66];;Ottawa_VANcouvER\n""".\\.Lufthansa.\\.""";[12, 33];20055.0;london_MONTreal\n'
+    t = TableParser(data, 4, [';','\n'])
+    t.to_upper('To_From')
+    t.remove_special_chars('Airline Code')
+    t.fix_flight_codes()
+    t.format_to_from()
+    t.print_table()
